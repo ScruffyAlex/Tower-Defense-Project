0074385317e1945a7f01907ff3f5fc6fe1e9a34e
JSchmidt1004 <60528235+Shiftyeyes654@users.noreply.github.com>
2020-12-02 17:58:27 -0700

    Not fixed.
    It's definitely not fixed. Taking a short break and looking at it again in like an hour

---
+++ TowerDefenseGame/Assets/Scripts/Projectile/Projectile.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public float speed = 10;
+    public Transform target;
+    private Vector3 targetDirection;
+
+    public void SetStaticDefaults()
+    {
+        target = gameObject.AddComponent<Transform>();
+    }
+
+    public void Create(Vector3 targetPosition)
+    {
+        targetDirection = targetPosition;
+    }
+
+    void Update()
+    {
+        Vector3 dir = targetDirection - transform.position;
+        GetComponent<Rigidbody>().velocity = dir.normalized * speed * Time.deltaTime;
+
+        //if (target)
+        //{
+        //    Vector3 dir = target.position - transform.position;
+        //    GetComponent<Rigidbody>().velocity = dir.normalized * speed * Time.deltaTime;
+        //}
+        //else
+        //{
+        //    Destroy(gameObject);
+        //}
+    }
+}
--- TowerDefenseGame/Assets/Scripts/Towers/LasagnaTower.cs
+++ TowerDefenseGame/Assets/Scripts/Towers/LasagnaTower.cs
@@ -4,9 +4,59 @@ using UnityEngine;
 
 public class LasagnaTower : Tower
 {
+<<<<<<< Updated upstream
     public override void Update()
     {
 
+=======
+    //public GameObject target;
+    float timer = 2.5f;
+    float shootSpeed = 3;
+    float shootTimer = 3.0f;
+    private float range = 5;
+    public GameObject projectile;
+
+    List<GameObject> enemies = new List<GameObject>();
+    public void GetEnemies()
+    {
+
+    }
+
+    public override void Update()
+    {
+        timer -= Time.deltaTime;
+        if(timer <= 0)
+        {
+            //timer = shootTimer;
+            //Collider2D[] colliders = Physics2D.OverlapCircleAll(new Vector2(transform.position.x, transform.position.y), range);
+
+            //foreach(Collider2D collider in colliders)
+            //{
+            //    if(collider != null)
+            //    { 
+            //        if (collider.GetComponent<BaseEnemy>() != null)
+            //        {
+            //            enemies.Add(collider.gameObject);
+            //        }
+            //    }
+            //}
+
+            //if(enemies[0] != null)
+            //{
+            //    target = enemies[0].gameObject;
+            //}
+            
+            //timer = 2.5f;
+        }
+
+        shootTimer -= Time.deltaTime;
+        if(shootTimer <= 0)// && target != null)
+        {
+            Shoot();
+            Debug.Log("Shoot");
+            shootTimer = shootSpeed;
+        }
+>>>>>>> Stashed changes
     }
 
     public override void Create()
@@ -21,6 +71,23 @@ public class LasagnaTower : Tower
 
     public override void Shoot()
     {
+<<<<<<< Updated upstream
+=======
+        //Projectile projectile = new Projectile { };
+        //projectile.Create(target.transform.position);
+
+        //projectile.target = transform;
+
+        //Projectile projectile = new Projectile { };
+        GameObject projectileClone = Instantiate(projectile, transform.position, Quaternion.identity);
+        //projectileClone.Create(new Vector3( 1, 1, 1 ));
+
+
+        //GameObject gameObject = (GameObject)Instantiate(projectile, transform.position, Quaternion.identity);
+        //Component comp = target.GetComponent<BaseEnemy>();
+        //gameObject.GetComponent<Projectile>().target = comp.transform;
+    }
+>>>>>>> Stashed changes
 
     }
 }
